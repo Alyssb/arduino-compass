f1d95dc20620ec689c50b53f5b741e0143ed930e
Alyssa Slayton <ajs41@missouristate.edu>
2020-01-28 13:34:32 -0600

    initial commit of added folders, still learning this

---
+++ just_averages.py
@@ -0,0 +1,18 @@
+'''
+Alyssa Slayton 01/28/2020
+'''
+'''
+A oython script which:
+    WILL:
+        get a list of all files in a folder
+        calculate averages for xaccel, yaccel, zaccel
+        return those averages in a list
+        put the averages in a file
+I will be using this data to calculate which direction random data is facing
+'''
+
+def main():
+    print("hello world.")
+
+if __name__ == '__main__':
+    main()
