a06ce9e3ab74ab4732a918b142e8337a6bcdc381
Alyssa Slayton <ajs41@missouristate.edu>
2020-01-29 12:05:31 -0600

    initial commit of the day. Automated creating filenames.txt

--- just_averages.py
+++ just_averages.py
@@ -10,18 +10,27 @@ A python script which:
         put the averages in a file
 I will be using this data to calculate which direction random data is facing
 '''
+'''
+Design notes for myself:
+    Get a list of all filenames in a folder
+    separate as filename_$accel_clean
+    gonna be a lot of files but that's chill ig
+'''
 from os import system, remove
 
-def getFiles():
-    print("not yet implemented")
+def getFiles(root, name):
+    system("dir /b /a-d " + root + name + " > " + root + name + "filenames.txt")
+    return(open(root + name + "filenames.txt").read().split())
 
 def splitByAccel():
     print("not yet implemented")
 
 def main():
     root = 'C:\\Users\\alyss\\Documents\\arduino-compass\\'
-    names = ["north", "south", "east", "west"]
-    
+    names = ["data_north\\", "data_south\\", "data_east\\", "data_west\\"]
+    for name in names:
+        filenames = getFiles(root, name)
+        print(filenames)
     print("hello world.")
 
 if __name__ == '__main__':
