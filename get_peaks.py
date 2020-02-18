6cac0a3db46424fbd1fc31bfa4c1d9f8f23ec709
Jo Slayton <ajs41@missouristate.edu>
2020-02-18 14:31:30 -0600

    got some organization for get_peaks.py

--- get_peaks.py
+++ get_peaks.py
@@ -1,4 +1,16 @@
 # will just get the averages from the sine curve.
+# going to try to not be too extra about it....
+
+numSteps = 80   # number of steps in the circle
+pointsPer = 30  # number of data points per step
+spacing = 10    # how many steps I want to go
+degrees = 4.5   # number degrees per step
+
+def getStep():
+    print("not yet implemented")
+
+def average():
+    print("not yet implemented")
 
 def main():
     print("hello world.")
