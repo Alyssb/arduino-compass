6b88798a0a0c4cae5b21ab4603e2296ab07f8b48
Jo Slayton <ajs41@missouristate.edu>
2020-05-19 14:58:51 -0500

    initial commit of calculate_direction.py

---
+++ calculate_direction.py
@@ -0,0 +1,12 @@
+'''
+Jo Slayton 2020
+
+Will calculate the direction of the sensor using a randomly generated x,y pair
+A concept test for the arduino implementation
+'''
+
+def main():
+    print("hello world.")
+
+if __name__ == "__main__":
+    main()
